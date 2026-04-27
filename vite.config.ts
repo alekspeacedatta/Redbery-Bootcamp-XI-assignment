dd9b8b6b5581f26c923bc2189959f833709853da
alexsandre <77684225+alekspeacedatta@users.noreply.github.com>
2026-04-27 05:06:37 +0400

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Redbery-Bootcamp-XI-assignment/',
+  base: '/Course-Marketplace/',
   plugins: [react(), tailwindcss(), svgr()],
   resolve: {
     alias: {
